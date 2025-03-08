378d98296120c6143d40adb61dde123fe059f22b
tiaprime <tiaprime@byu.edu>
2025-03-07 18:07:59 -0700

    filled out the bage and made functions

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import data from './CollegeBasketballTeams.json';
 import './App.css'
 
+
+console.log("HEY LOOK AT ME", data)
+
+function Welcome(){
+  return(<h1>This is THEE march madness page!!! (something I could NOT care less about!!!)</h1>);
+}
+
+
+function TeamInfo({School, mascot, city ,state} : {School: string; mascot:string; city:string; state:string}){
+
+  return(
+    <>
+      <img />
+      <h2>------{School}------</h2>
+      <h3>The Mascot is... {mascot}!</h3>
+      <h3>Location: {city}, {state}</h3>
+    </>
+  );
+}
+
+// Show all teams
+function AllTeams(){
+  return(
+    // These are react cragments, it fuctions like a div
+    <> 
+      {/* THIS is like a loop  */}
+      {data.teams.map((team) => (
+        // this is different because the code is ONLY looking for what I listed. also it clearly shows the property and varial realtion
+        <TeamInfo  School={team.school} mascot={team.name} city={team.city} state={team.state}/>
+      ))}
+      </>
+
+  )
+}
+
+
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Welcome />
+      <h2>-------------------------------------All Teams-------------------------------------------</h2>
+      <AllTeams />
     </>
   )
 }
